5c79b3afb9833fa3fa4ddb6beedd3b711b03c0f3
Karunkumar <kkarun701@gmail.com>
2025-12-16 22:36:39 +0530

    Initial Commit

---
+++ src/test/java/com/tailora/TailoraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tailora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TailoraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
